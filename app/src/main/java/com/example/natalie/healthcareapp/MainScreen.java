a5c710468fc97e349c30d3ba6060c368d8be96d9
chenlk <chenlk@mcmaster.ca>
2016-01-16 19:56:36 -0500

    Linked Pages
    Drug List page is working now

--- app/src/main/java/com/example/natalie/healthcareapp/MainScreen.java
+++ app/src/main/java/com/example/natalie/healthcareapp/MainScreen.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -48,5 +50,13 @@ public class MainScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void openPage(View view){
+        //do something in response to button click
+        Intent drugPage = new Intent(this,DrugList.class);
+        Log.e("test", "Button is clicked");
+        startActivity(drugPage);
+
+
     }
 }
